cc4f5caec3aa0db9ac9a132e499df962e844790d
DaniAm94 <dani.amico94@gmail.com>
2024-02-26 17:07:20 +0100

    added private product's attribute and private method to set its value plus added price attribute

--- models/product.php
+++ models/product.php
@@ -1,16 +1,28 @@
 <?php
 
 class Product{
+    private $id;
     public $label;
     public $img;
     public $type;
     public $category;
+    public $price;
 
-   public  function __construct(string $label, string $img, string $type, Category $category){
+   public  function __construct(string $label, string $img, string $type, Category $category, float $price= 0.0){
 
+    $this->setId();
     $this->label = $label;
     $this->img = $img;
     $this->type = $type;
     $this->category = $category;
+    $this->price = $price . '€';
+    }
+
+    public function getId(){
+        return $this->id;
+    }
+
+    private function setId(){
+       $this->id= substr(uniqid(), -4);
     }
 }
